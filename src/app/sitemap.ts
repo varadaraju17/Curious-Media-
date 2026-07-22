22f41bf0bb2c447b2c032421f4ed4c1f4edcf418
Darshan D <182134264+varadaraju17@users.noreply.github.com>
2026-07-23 00:39:15 +0530

    fix: export const dynamic = 'force-static' in robots and sitemap for static html export

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+
 const BASE_URL = 'https://curiousmedia.in';
 
 export default function sitemap(): MetadataRoute.Sitemap {
